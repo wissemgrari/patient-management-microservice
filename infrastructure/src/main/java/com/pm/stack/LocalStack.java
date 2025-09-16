0d5b6534458c19c7c0024b62ec97853fec9c33dd
wissemgrari <wissemgrari@outlook.com>
2025-09-16 18:18:53 +0100

    feat: add VPC configuration for LocalStack setup

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -5,11 +5,15 @@ import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
+  private final Vpc vpc;
+
   public LocalStack(final App scope, final String id, final StackProps props) {
     super(scope, id, props);
+    this.vpc = createVpc();
   }
 
   public static void main(String[] args) {
@@ -24,6 +28,15 @@ public class LocalStack extends Stack {
     new LocalStack(app, "localstack", props);
     app.synth();
     System.out.println("App synthesizing in progress...");
+  }
+
+  private Vpc createVpc() {
+    return Vpc.Builder
+        .create(this, "PatientManagementVPC")
+        .vpcName("PatientManagementVPC")
+        .maxAzs(2)
+        .build();
+
   }
 
 }
