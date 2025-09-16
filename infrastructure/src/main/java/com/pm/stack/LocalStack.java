0362290c47b576533db0bc887b92e6d6919774b1
wissemgrari <wissemgrari@outlook.com>
2025-09-16 19:22:23 +0100

    feat: add RDS instances and health checks for authentication and patient services

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -3,9 +3,20 @@ package com.pm.stack;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.Token;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.Credentials;
+import software.amazon.awscdk.services.rds.DatabaseInstance;
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
+import software.amazon.awscdk.services.rds.PostgresEngineVersion;
+import software.amazon.awscdk.services.rds.PostgresInstanceEngineProps;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
 
@@ -14,6 +25,18 @@ public class LocalStack extends Stack {
   public LocalStack(final App scope, final String id, final StackProps props) {
     super(scope, id, props);
     this.vpc = createVpc();
+
+    DatabaseInstance authServiceDb = createDatabaseInstance("AuthServiceDB", "auth-service-db");
+
+    DatabaseInstance patientServiceDb = createDatabaseInstance("PatientServiceDB",
+        "patient-service-db");
+
+    CfnHealthCheck authDBHealthCheck = createDBHealthCheck(authServiceDb,
+        "AuthServiceDBHealthCheck");
+
+    CfnHealthCheck patientDBHealthCheck = createDBHealthCheck(patientServiceDb,
+        "PatientServiceDBHealthCheck");
+
   }
 
   public static void main(String[] args) {
@@ -21,9 +44,7 @@ public class LocalStack extends Stack {
 
     // Use BootstraplessSynthesizer() for LocalStack to avoid AWS CDK bootstrapping.
     // This enables local development and testing without deploying resources to AWS.
-    StackProps props = StackProps.builder()
-        .synthesizer(new BootstraplessSynthesizer())
-        .build();
+    StackProps props = StackProps.builder().synthesizer(new BootstraplessSynthesizer()).build();
 
     new LocalStack(app, "localstack", props);
     app.synth();
@@ -31,12 +52,37 @@ public class LocalStack extends Stack {
   }
 
   private Vpc createVpc() {
-    return Vpc.Builder
-        .create(this, "PatientManagementVPC")
+    return Vpc.Builder.create(this, "PatientManagementVPC")
         .vpcName("PatientManagementVPC")
         .maxAzs(2)
         .build();
 
+  }
+
+  private DatabaseInstance createDatabaseInstance(String id, String dbName) {
+    return DatabaseInstance.Builder.create(this, id)
+        .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+            .version(PostgresEngineVersion.VER_17_2)
+            .build()))
+        .vpc(vpc)
+        .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+        .allocatedStorage(20)
+        .credentials(Credentials.fromGeneratedSecret("admin_user"))
+        .databaseName(dbName)
+        .removalPolicy(RemovalPolicy.DESTROY)
+        .build();
+  }
+
+  private CfnHealthCheck createDBHealthCheck(DatabaseInstance db, String id) {
+    return CfnHealthCheck.Builder.create(this, id)
+        .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+            .type("TCP")
+            .port(Token.asNumber(db.getDbInstanceEndpointPort()))
+            .ipAddress(db.getDbInstanceEndpointAddress())
+            .requestInterval(30)
+            .failureThreshold(3)
+            .build())
+        .build();
   }
 
 }
