6712f1fba330369732d6a3c941afb27a1cd67a55
wissemgrari <wissemgrari@outlook.com>
2025-09-17 19:20:28 +0100

    feat: add API Gateway service with Fargate configuration

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -26,6 +27,7 @@ import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
 import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.PortMapping;
 import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
@@ -91,6 +93,8 @@ public class LocalStack extends Stack {
     patientService.getNode().addDependency(patientDBHealthCheck);
     patientService.getNode().addDependency(billingService);
     patientService.getNode().addDependency(mskCluster);
+
+    createApiGatewayService();
 
   }
 
@@ -215,6 +219,49 @@ public class LocalStack extends Stack {
         .assignPublicIp(false)
         .serviceName(imageName)
         .build();
+  }
+
+  private void createApiGatewayService() {
+    FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder
+        .create(this, "APIGatewayTaskDefinition")
+        .cpu(256)
+        .memoryLimitMiB(512)
+        .build();
+
+    ContainerDefinitionOptions containerOptions = ContainerDefinitionOptions.builder()
+        .image(ContainerImage.fromRegistry("api-gateway"))
+        .environment(Map.of(
+            "SPRING_PROFILES_ACTIVE", "prod",
+            "AUTH_SERVICE_URL", "http://host.docker.internal:4005"
+        ))
+        .portMappings(List.of(4004).stream()
+            .map(port -> PortMapping.builder()
+                .containerPort(port)
+                .hostPort(port).protocol(Protocol.TCP)
+                .build())
+            .toList())
+        .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+            .logGroup(LogGroup.Builder.create(this, "APIGatewayLogGroup")
+                .logGroupName("/ecs/api-gateway")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .retention(RetentionDays.ONE_DAY)
+                .build())
+            .streamPrefix("api-gateway")
+            .build()))
+        .build();
+
+    taskDefinition.addContainer("APIGatewayContainer", containerOptions);
+
+    ApplicationLoadBalancedFargateService apiGateway =
+        ApplicationLoadBalancedFargateService.Builder
+            .create(this, "APIGatewayService")
+            .cluster(ecsCluster)
+            .serviceName("api-gateway")
+            .taskDefinition(taskDefinition)
+            .desiredCount(1)
+            .healthCheckGracePeriod(Duration.seconds(60))
+            .build();
+
   }
 
   public static void main(String[] args) {
